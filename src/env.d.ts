cc16414ac200965ef08572341573224d4ba92b06
Apirak Panatkool <apirakb@gmail.com>
2024-12-08 08:02:20 +0700

    update image

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,17 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
