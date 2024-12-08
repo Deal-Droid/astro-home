b2cb44c48bf36e6b23cbf2260690d559e1bff233
Apirak Panatkool <apirakb@gmail.com>
2024-12-08 07:12:31 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship-pro.web3templates.com",
+  site: "https://deal-droid.github.io",
+  base: "/astro-home",
   image: {
     domains: ["source.unsplash.com", "images.unsplash.com"],
   },
