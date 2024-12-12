d831fdfb703492f35670a0a1cd477f82e9428ab0
Apirak Panatkool <apirakb@gmail.com>
2024-12-12 20:26:03 +0700

    refactor ♻️(i18n): update footer and navbar translations, add missing home/blog/document/contact translations
    - Refactor footer component to use localized translations and improve structure.
    - Refactor navbar translations and simplify menu item titles.
    - Added missing translations for home, blog, document, and contact in both English and Thai.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -18,9 +18,17 @@ export const defaultLang = 'en';
 
 export const ui: UILanguages = {
   en: {
-    'general.start_trial': 'Start 14-day trial'
+    'general.start_trial': 'Start 14-day trial',
+    'general.home': 'Home',
+    'general.blog': 'Blog',
+    'general.document': 'Document',
+    'general.contact': 'Contact',
   },
   th: {
     'general.start_trial': 'ฟรี 14 วัน - ใช้เลย',
+    'general.home': 'หน้าหลัก',
+    'general.blog': 'บล็อก',
+    'general.document': 'คู่มือ',
+    'general.contact': 'ติดต่อเรา',
   },
 } as const;
