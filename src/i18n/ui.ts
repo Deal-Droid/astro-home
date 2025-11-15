fec0e0852cb7c453f8fdf426880e09f8def12b29
Apirak Panatkool <apirakb@gmail.com>
2025-11-15 11:11:28 +0700

    feat(navbar): add features menu item with translation support

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,38 +1,40 @@
 type TranslationKeys = {
   [key: string]: string;
-}
+};
 
 export type UILanguages = {
   en: TranslationKeys;
   th: TranslationKeys;
-}
+};
 
 export const languages = {
-  en: 'English',
-  th: 'ไทย',
+  en: "English",
+  th: "ไทย",
 };
 
 export const showDefaultLang = false;
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui: UILanguages = {
   en: {
-    'general.start_trial': 'Start 14-day trial',
-    'general.home': 'Home',
-    'general.terms': 'Term and Condition',
-    'general.privacy': 'Privacy Policy',
-    'general.blog': 'Blog',
-    'general.document': 'Document',
-    'general.contact': 'Contact',
+    "general.start_trial": "Start 14-day trial",
+    "general.home": "Home",
+    "general.features": "Features",
+    "general.terms": "Term and Condition",
+    "general.privacy": "Privacy Policy",
+    "general.blog": "Blog",
+    "general.document": "Document",
+    "general.contact": "Contact",
   },
   th: {
-    'general.start_trial': 'ฟรี 14 วัน - ใช้เลย',
-    'general.home': 'หน้าหลัก',
-    'general.terms': 'ข้อตกลงและข้อตกลง',
-    'general.privacy': 'นโยบายความเป็นส่วนตัว',
-    'general.blog': 'บล็อก',
-    'general.document': 'คู่มือ',
-    'general.contact': 'ติดต่อเรา',
+    "general.start_trial": "ฟรี 14 วัน - ใช้เลย",
+    "general.home": "หน้าหลัก",
+    "general.features": "คุณสมบัติ",
+    "general.terms": "ข้อตกลงและข้อตกลง",
+    "general.privacy": "นโยบายความเป็นส่วนตัว",
+    "general.blog": "บล็อก",
+    "general.document": "คู่มือ",
+    "general.contact": "ติดต่อเรา",
   },
 } as const;
