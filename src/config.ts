a41d8c53b1d388e9252839b1454c135206a3a051
Apirak Panatkool <apirakb@gmail.com>
2026-04-06 12:28:39 +0700

    feat: update retail components to use dynamic registration links and improve configuration management

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,13 @@
 export const config = {
   urls: {
     // register: "https://app.dealdroid.net/register",
-    register: "https://accounts.dealdroid.net/sign-up"
-  }
+    register: "https://accounts.dealdroid.net/sign-up",
+  },
+  retail: {
+    register: {
+      en: "https://go.dealdroid.ai/en/register",
+      th: "https://go.dealdroid.ai/th/register",
+    },
+    line: "https://line.me/R/ti/p/@dealdroid",
+  },
 } as const;
